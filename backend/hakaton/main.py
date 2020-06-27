f8676cde09c960abe677dae714eba69cc0fe3418
John <john@Johns-MacBook-Pro.local>
2020-06-27 13:05:38 +0300

    response schemas

--- backend/hakaton/main.py
+++ backend/hakaton/main.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask import request, Response
+from flask import request
+from marshmallow import ValidationError
 
 from . import services
-
+from .schemas import TypeSchema
 
 app = Flask(__name__)
 
@@ -10,36 +11,39 @@ app = Flask(__name__)
 @app.route('/api/types', methods=['GET', ])
 def get_types():
     return {
-        'types': [
-            {
-                'name': 'string',
-            },
-            {
-                'name': 'integer',
-            },
-        ]
+        'types': TypeSchema().dump(services.get_types(), many=True)
     }
 
 
 @app.route('/api/types', methods=['POST', ])
 def add_type():
-    type = request.get_json()
-    return services.create_type(type)
+    try:
+        new_type = TypeSchema().load(request.get_json())
+    except ValidationError as err:
+        return err.messages, 400
+
+    saved_type = services.create_type(new_type)
+    return TypeSchema().dump(saved_type)
 
 
-@app.route('/api/types/<str:name>', methods=['PUT', ])
+@app.route('/api/types/<string:name>/update', methods=['PUT', ])
 def update_type(name):
     type = services.get_type(name)
-    return services.update_type(type)
+    try:
+        saved_type = services.update_type(type)
+    except ValidationError as err:
+        return err.messages, 400
+
+    return TypeSchema().dump(saved_type)
 
 
-@app.route('/api/types/<str:name>', methods=['DELETE', ])
+@app.route('/api/types/<string:name>/delete', methods=['DELETE', ])
 def delete_type(name: str):
-    type = services.delete_type(name)
-    return Response(status=204)
+    services.delete_type(name)
+    return {}, 204
 
 
-@app.route('/api/types/<str:name>/export/xsd', methods=['GET', ])
+@app.route('/api/types/<string:name>/export/xsd', methods=['GET', ])
 def export_type(name: str):
     return {
 
